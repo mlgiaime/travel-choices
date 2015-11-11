28d75a6c7256b8860611e07ca7fa6b1ec972df96
Melissa Giaime <melissa.giaime@gmail.com>
2015-11-10 17:26:09 -0800

    JS file functional, but total votes is not working yet. HTML file nearly finished, CSS is not started other than the very basics

--- travel.js
+++ travel.js
@@ -1,37 +1,44 @@
+var goingAway = [];
 var trackIt = {
 
-
-//randomizes the order of photos
-
-calcRandom = function() {
+calcRandom: function () {
   return Math.floor(Math.random() * goingAway.length);
-}
+},
 
 //makes sure that the page will return to the calcRandom function to grab the next
 //photo... if pic1 === pic2 then go back and find another one!
-calcNextPlace = function(location1){
+
+calcNextPlace: function (location1) {
   do {
-    var location2 = calcRandom();
+    var location2 = trackIt.calcRandom();
   }
   while (location1 === location2)
     return location2;
-}
+},
+// console.log(calcNextPlace());
 
 //allows the photos to be shown on the page to the user
-displayAdventures = function() {
-  var location1 = calcRandom();
-  var location2 = calcNextPlace(location1);
-  var choice = document.getElementById(choice);
-}
+displayAdventures: function () {
+  var index1 = trackIt.calcRandom();
+  var index2 = trackIt.calcNextPlace(index1);
+  // var choice = document.getElementById('choice');
+  head1.innerHTML = location1.place;
+  pic1.setAttribute('src', goingAway[index1].path);
+
+  head2.innerHTML = location2.place;
+  pic2.setAttribute('src', goingAway[index2].path);
+},
 
 //tells the js function to remove the first image choice from the potential options
 //to prevent having the same photo displayed together
-vote = function(location) {
+vote: function (location) {
+  console.log("this event happened");
   goingAway[location].votes++;
-  pic1.parentNode.removeChild(pic1);
-  pic2.parentNode.removeChild(pic2);
-  displayAdventures();
+  // pic1.parentNode.removeChild(pic1);
+  // pic2.parentNode.removeChild(pic2);
+  trackIt.displayAdventures();
 }
+};
 
 function Adventure (place, path) {
   this.place = place;
@@ -40,33 +47,6 @@ function Adventure (place, path) {
   goingAway.push(this);
 }
 
-//sets up the js file to pull photos from the 'images' folder to display images
-//on the page as well as adding in photo dimensions
-pic1 = document.getElementById('img');
-pic2 = document.getElementById('img');
-
-pic1.setAttribute('id', 'image1');
-pic2.setAttribute('id', 'image2')
-
-pic1.setAttribute('src', goingAway[location1].path);
-pic2.setAttribute('src', goingAway[location2].path);
-
-pic1.setAttribute('width', '500');
-pic2.setAttribute('width', '500')
-
-choice.appendChild(pic1);
-choice.appendChild(pic2);
-
-
-//event listener adds in the click result for each question
-pic1.addedEventListener('click', function() {
-  vote(location1);
-  });
-pic2.addedEventListener('click', function() {
-  vote(location2);
-  });
-
-//all objects for page
 new Adventure('Angkor Wat', 'images/angkorwat.jpg');
 new Adventure('Arches National Park', 'images/arches.jpg');
 new Adventure('Big Ben and Westminster Abbey', 'images/bigben.jpg');
@@ -90,7 +70,38 @@ new Adventure('Sydney Opera House', 'images/sydney.jpg');
 new Adventure('Taj Mahal', 'images/tajmahal.jpg');
 new Adventure('Yellowstone National Park', 'images/yellowstone.jpg');
 
+var location1 = trackIt.calcRandom();
+var location2 = trackIt.calcNextPlace(location1);
+
+//sets up the js file to pull photos from the 'images' folder to display images
+//on the page as well as adding in photo dimensions
+var pic1 = document.getElementById('location1photo');
+var pic2 = document.getElementById('location2photo');
+var adventure1 = document.getElementById('location1');
+var adventure2 = document.getElementById('location2');
+
+// pic1.setAttribute('id', 'location1photo');
+// pic2.setAttribute('id', 'location2photo');
+
+// pic1.setAttribute('src', goingAway[location1].path);
+// pic2.setAttribute('src', goingAway[location2].path);
+
+// pic1.setAttribute('width', '500');
+// pic2.setAttribute('width', '500');
+
+// trackIt.appendChild(pic1);
+// trackIt.choice.appendChild(pic2);
+
+var head1 = document.getElementById('location1header');
+var head2 = document.getElementById('location2header');
 
 
+//event listener adds in the click result for each question
+pic1.addEventListener('click', function() {
+  trackIt.vote(location1);
+  });
+pic2.addEventListener('click', function() {
+  trackIt.vote(location2);
+  });
 
-
+trackIt.displayAdventures();
